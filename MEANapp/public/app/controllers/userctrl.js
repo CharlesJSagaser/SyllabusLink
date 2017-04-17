bfd33e3f158264ae2adcb703832b0b65d6083ee4
Syrus <syrusyeung@gmail.com>
2017-04-17 15:37:27 -0600

    minor changes

--- MEANapp/public/app/controllers/userctrl.js
+++ MEANapp/public/app/controllers/userctrl.js
@@ -1,9 +1,12 @@
 angular.module('userControllers', [])
 
-.controller('regCtrl', function(){
+.controller('regCtrl', function($http){
 
 		this.regUser = function(regData) {
 		console.log('Form Submitted');
 		console.log(this.regData);
+		$http.post('api/users', this.regData).then(function(data){
+			console.log(data);
+		});
 	};
 });
