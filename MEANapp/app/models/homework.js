80fc1cc309668daf06c7b449e86342c5da0a569e
GhostEagle <xavierperches@yahoo.com>
2017-05-02 13:14:03 -0600

    Adding homework.js file

--- MEANapp/app/models/homework.js
+++ MEANapp/app/models/homework.js
@@ -1,22 +1,22 @@
 //creates a template for a homework
 
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-var bcrypt = require('bcrypt-nodejs');
+//var mongoose = require('mongoose');
+//var Schema = mongoose.Schema;
+//var bcrypt = require('bcrypt-nodejs');
 
-var SyllabusSchema = new Schema({
-  hwname: { type: String, required: true},
-  material: { type: String},
-  assigned: {type:Date, required: true},
-  due: {type:Date, required: true},
-
-
-});
+//var SyllabusSchema = new Schema({
+//  hwname: { type: String, required: true},
+//  material: { type: String},
+//  assigned: {type:Date, required: true},
+//  due: {type:Date, required: true},
 
 
+//});
 
 
-module.exports = mongoose.model('Homework', HomeworkSchema);
+
+
+//module.exports = mongoose.model('Homework', HomeworkSchema);
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
